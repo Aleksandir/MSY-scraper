10bc591af4caa0acc9edbcfb9140fba05d9305a0
Aleks B <aleksandir@me.com>
2023-12-23 21:14:07 +1030

    Refactor SiteUrlScraper.py to improve readability and type annotations

--- src/SiteUrlScraper.py
+++ src/SiteUrlScraper.py
@@ -6,18 +6,20 @@ def main():
     soup = get_page()
     soup = BeautifulSoup(soup.content, "html.parser")
 
-    div = soup.find_all("div", {"class": "ovhide productsIn productsz"})
+    divs = soup.find_all("div", {"class": "ovhide productsIn productsz"})
 
-    links = []
-    for div in div:
-        links.append(div.find("a")["href"] if div else None)
+    links: list[str] = []
+    for div in divs:
+        links.append(div.find("a")["href"] if div else None)  # type: ignore
 
     with open("links.txt", "w") as f:
         for link in links:
             f.write(f"{link}\n")
 
 
-def get_page(url: str = "https://www.msy.com.au/categories"):
+def get_page(
+    url: str = "https://www.msy.com.au/categories",
+) -> requests.models.Response:
     page = requests.get(url)
     return page
 
