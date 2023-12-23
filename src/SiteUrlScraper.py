6c43bb002d73cb2d32c5078b03d68eaf3d44bee6
Aleks B <aleksandir@me.com>
2023-12-23 23:08:29 +1030

    Add link scraping functionality to SiteUrlScraper.py

--- src/SiteUrlScraper.py
+++ src/SiteUrlScraper.py
@@ -3,15 +3,21 @@ from bs4 import BeautifulSoup
 
 
 def main():
+    # scrape links from categories page
     soup = get_page("https://www.msy.com.au/categories")
+    # parse html content
     soup = BeautifulSoup(soup.content, "html.parser")
 
+    # find all divs with class "ovhide productsIn productsz"
     divs = soup.find_all("div", {"class": "ovhide productsIn productsz"})
 
+    # list to store links
     links: list[str] = []
+    # for each div, get the link and append to list
     for div in divs:
         links.append(div.find("a")["href"] if div else None)  # type: ignore
 
+    # write links to file for later use
     with open("data/links.txt", "w") as f:
         for link in links:
             f.write(f"https://www.msy.com.au/{link}\n")
