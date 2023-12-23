e575ae3c49eee4bf497b194ecac4b50a6406b02c
Aleks B <aleksandir@me.com>
2023-12-23 22:43:45 +1030

    Refactor scraper.py to extract get_products_from_page function

--- src/scraper.py
+++ src/scraper.py
@@ -7,36 +7,42 @@ from tqdm import tqdm
 
 def main():
     all_products = []
-    coreURL = "https://www.msy.com.au"
     links = get_links("data/links.txt")
 
     for link in tqdm(links):
-        soup = get_page(link)
-
-        soup = BeautifulSoup(soup.content, "html.parser")
-
-        divs = soup.find_all("li", {"class": "goods_info"})
-        # for each product on page
-        for div in divs:
-            # store product info in dict
-            product = {}
-            try:
-                product["name"] = div.find("span", {"itemprop": "name"}).text
-                product["price"] = div.find("span", {"itemprop": "price"})["content"]
-                # get link and add to coreURL to make absolute link
-                product["link"] = coreURL + div.find("a")["href"]
-                product["stock"] = div.find(
-                    "span", {"class": "goods_stock graphik-bold"}
-                ).text
-            except TypeError:
-                continue
-
-            # add product to list
-            all_products.append(product)
+        all_products.append(get_products_from_page(link))
 
     # write products to file
     with open("data/products.json", "a") as f:
         json.dump([all_products], f, indent=4)
+
+
+def get_products_from_page(link: str) -> list[dict]:
+    coreURL = "https://www.msy.com.au"
+    products = []
+    soup = get_page(link)
+
+    soup = BeautifulSoup(soup.content, "html.parser")
+
+    divs = soup.find_all("li", {"class": "goods_info"})
+    # for each product on page
+    for div in divs:
+        # store product info in dict
+        product = {}
+        try:
+            product["name"] = div.find("span", {"itemprop": "name"}).text
+            product["price"] = div.find("span", {"itemprop": "price"})["content"]
+            # get link and add to coreURL to make absolute link
+            product["link"] = coreURL + div.find("a")["href"]
+            product["stock"] = div.find(
+                "span", {"class": "goods_stock graphik-bold"}
+            ).text
+        except TypeError:
+            continue
+
+        products.append(product)
+
+    return products
 
 
 def get_links(file: str) -> list[str]:
